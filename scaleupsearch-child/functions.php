6dd158f9dd593073c88dcf9d423350f38a1d349e
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-27 23:28:12 +0800

    Update functions.php

--- scaleupsearch-child/functions.php
+++ scaleupsearch-child/functions.php
@@ -3,6 +3,10 @@ add_action('wp_enqueue_scripts', 'scaleupsearch_child_enqueue_styles');
 function scaleupsearch_child_enqueue_styles()
 {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	if (is_post_type_archive('careers') ) {
+		wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+		wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+	}
 }
 
 require_once('includes/shortcodes.php');
