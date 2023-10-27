2416668597efff250a9390fb939e394a10758758
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-27 21:17:22 +0800

    cc

--- scaleupsearch-child/functions.php
+++ scaleupsearch-child/functions.php
@@ -1,6 +1,8 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'scaleupsearch_child_enqueue_styles' );
-	 function scaleupsearch_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+<?php
+add_action('wp_enqueue_scripts', 'scaleupsearch_child_enqueue_styles');
+function scaleupsearch_child_enqueue_styles()
+{
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+
+require_once('includes/shortcodes.php');
---
+++ scaleupsearch-child/includes/shortcodes.php
@@ -0,0 +1,20 @@
+<?php
+
+function breadcrumbs() {
+    ob_start();
+    ?>
+    <div class="breadrumbs">
+        <ul>
+            <li>
+                <a href="<?= get_site_url() ?>">Home</a>
+            </li>
+            <li>
+                <span href=""><?= get_the_title() ?></span>
+            </li>
+        </ul>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
+add_shortcode('breadcrumbs', 'breadcrumbs');
