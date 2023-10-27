9547fbf7871ad5ca7d8eea540b226c3370a773a4
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-27 22:54:27 +0800

    Update post-meta.php

--- scaleupsearch-child/includes/post-meta.php
+++ scaleupsearch-child/includes/post-meta.php
@@ -31,20 +31,9 @@ Container::make('post_meta', __('Careers Details'))
     ->where('post_type', '=', 'careers')
     ->add_fields(array(
         Field::make('text', 'salary', 'Salary'),
-        Field::make('complex', 'accordion', 'Accordion')
-            ->add_fields(array(
-                Field::make('text', 'accordion_title', 'Accordion Title'),
-                Field::make('rich_text', 'accordion_content', 'Accordion Content'),
-            ))
-            ->set_default_value(array(
-                array(
-                    'accordion_title' => 'Responsibilities',
-                ),
-                array(
-                    'accordion_title' => 'Requirements',
-                ),
-            ))
-            ->set_header_template('<%- accordion_title  %>')
+        Field::make('text', 'location', 'Location'),
+        Field::make('text', 'description', 'Description'),
+        
     ));
 
 
