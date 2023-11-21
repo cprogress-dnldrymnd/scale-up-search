ef173ac789c3a5acc445ab15607d06bb90a97450
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-21 19:59:43 +0800

    Update post-meta.php

--- scaleupsearch-child/includes/post-meta.php
+++ scaleupsearch-child/includes/post-meta.php
@@ -32,6 +32,7 @@ Container::make('post_meta', __('Job Listing Details'))
     ->add_fields(array(
         Field::make('text', 'salary', 'Salary'),
         Field::make('text', 'location', 'Location'),
+        Field::make('text', 'email_address', 'Email address to send enquiry'),
         
     ));
 
