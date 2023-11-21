b6c24fb17e053bb2b806aaada77d85193045ca61
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-21 19:46:20 +0800

    Update functions.php

--- scaleupsearch-child/functions.php
+++ scaleupsearch-child/functions.php
@@ -28,9 +28,11 @@ function action_wp_footer()
 	if (isset($_GET['form']) && $_GET['form'] == 'find-a-role') {
 ?>
 		<script>
-			setTimeout(function() {
-				jQuery('#elementor-tab-title-8222').click();
-			}, 1000);
+			jQuery(document).ready(function() {
+				setTimeout(function() {
+					jQuery('#elementor-tab-title-8222').click();
+				}, 300);
+			});
 		</script>
 <?php
 	}
