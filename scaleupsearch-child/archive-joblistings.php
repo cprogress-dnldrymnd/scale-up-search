5c4d568a18343691f4b5466267d4b79e2be392bf
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-21 19:51:07 +0800

    cs

--- scaleupsearch-child/archive-joblistings.php
+++ scaleupsearch-child/archive-joblistings.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 
 <?= do_shortcode("[hfe_template id='12764']") ?>
-<?php get_template_part('template-parts/careers') ?>
+<?php get_template_part('template-parts/job-listings') ?>
 
 <?= do_shortcode("[hfe_template id='12765']") ?>
 <?php get_footer(); ?>
